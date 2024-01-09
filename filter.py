3a1da3e1a352128825cd59a0e80febc75fcc5bfd
Rohith V <104898769+imrohith187@users.noreply.github.com>
2024-01-09 14:17:39 +0530

    filter.py

--- filter.py
+++ filter.py
@@ -2,9 +2,12 @@ def filter(arr,target):
     for val in arr:
         if(val==target):
             return display_result(val)
-        return False
+        return display_result(False)
 
 def display_result(val):
+    if(val==False):
+        print("Element not Found")
+        return 0
     print(val+" is Found")
     
 arr = ["one","two","three"]
