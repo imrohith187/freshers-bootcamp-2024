8607867f755f85716ede75df6bae37b7f4f6abb3
Rohith V <104898769+imrohith187@users.noreply.github.com>
2024-01-10 15:33:57 +0530

    Create compositionfilter.py

---
+++ compositionfilter.py
@@ -0,0 +1,57 @@
+import java.util.*;
+
+class ConsoleDisplayController{
+    private ArrayList<String> result;
+    
+    public void setContent (ArrayList<String>msg){
+        this.content = msg;
+    }
+    
+    public void display(){
+        System.out.print(result.toString());
+    }
+}
+
+class LengthAsStrategy{
+    private Integer lengthChar;
+    
+    public void setLengthOfChar(Integer len){
+        this.lengthChar = len;
+    }
+    
+    public boolean invoke(String item){
+        if(item.length()<lengthChar){
+            return true;
+        }
+        return false;
+    }
+}
+
+class StringListFilterController{
+    LengthAsStrategy predicate = new LengthAsStrategy();
+    public ArrayList<String> filter(ArrayList<String> strlist){
+        ArrayList<String> filteredArr = new ArrayList<>();
+        predicate.setLengthOfChar(4);
+        for(String word:strlist){
+            if(predicate.invoke(word))filteredArr.add(word);
+        }
+        return filteredArr;
+    }
+}
+
+public class Main{
+    public static void main(String args[]){
+        ArrayList<String> = new ArrayList<>();
+        
+        arr.add("one");
+        arr.add("two");
+        arr.add("three");
+        StringListFilterController stringListFilterControllerObj = new StringListFilterController();
+        ArrayList<String> filteredArray = stringListFilterControllerObj.filter(arr);
+      
+        ConsoleDisplayController consoleDisplayControllerObj = new ConsoleDisplayController();
+        consoleDisplayControllerObj.setContent(filteredArray);
+        consoleDisplayControllerObj.display();
+        
+    }
+}
